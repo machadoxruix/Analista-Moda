b2e6eace70a42bd9a80d09d03d05f6e2b8bf6fb6
machadoxruix <135983537+machadoxruix@users.noreply.github.com>
2026-07-13 20:20:20 +0000

    arregla

--- portada0.php
+++ portada0.php
@@ -136,8 +136,7 @@ $abrir_registro = (!empty($error_registro)) ? 'true' : 'false';
   </ul>
   <div class="nav-right">
     <?php if (isset($_SESSION['nombre_usuario'])): ?>
-        <a href="historial.php" class="nav-btn-start">MI HISTORIAL</a>
-    <?php endif; ?>
+    <a href="historial.php" class="nav-btn-historial">MI HISTORIAL</a>    <?php endif; ?>
     <a href="index.php" class="nav-btn-start">COMENZAR</a>
     <button class="btn-user" id="btnUser" aria-label="Abrir panel de usuario">
       <svg viewBox="0 0 24 24"><circle cx="12" cy="8" r="4"/><path d="M4 20c0-4 3.6-7 8-7s8 3 8 7"/></svg>
